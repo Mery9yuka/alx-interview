2c967078562078e04188d451468cc5c82165a7ef
Mery9yuka <benhaida.maryem@gmail.com>
2024-08-06 17:51:59 +0000

    class

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,14 +6,14 @@ def pascal_triangle(n):
     '''
     Pascal's triangle function
     '''
-    List = []
+    lists = []
     if n == 0:
-        return List
-    for k in range(n):
-        List.append([])
-        List[k].append(1)
-        if (k > 0):
-            for m in range(1, k):
-                lists[k].append(lists[k - 1][m - 1] + lists[k - 1][m])
-            lists[k].append(1)
-    return List
+        return lists
+    for i in range(n):
+        lists.append([])
+        lists[i].append(1)
+        if (i > 0):
+            for j in range(1, i):
+                lists[i].append(lists[i - 1][j - 1] + lists[i - 1][j])
+            lists[i].append(1)
+    return lists
