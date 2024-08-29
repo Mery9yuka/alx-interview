549361e1d48ef033b75c3b466c4bdd6c70b09b4b
Mery9yuka <benhaida.maryem@gmail.com>
2024-08-29 22:25:36 +0000

    pycodestyle

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,6 +2,7 @@
 # a program that solves the N queens problem.
 import sys
 
+
 def is_safe(board, row, col):
     """
     function that check if it's safe to place a queen at board[row][col]
@@ -9,16 +10,17 @@ def is_safe(board, row, col):
     for i in range(row):
         if board[i] == col:
             return False
-    
+
     for i, j in zip(range(row - 1, -1, -1), range(col - 1, -1, -1)):
         if board[i] == j:
             return False
-    
+
     for i, j in zip(range(row - 1, -1, -1), range(col + 1, len(board))):
         if board[i] == j:
             return False
 
     return True
+
 
 def solve_nqueens(board, row):
     """
@@ -28,12 +30,13 @@ def solve_nqueens(board, row):
     if row == N:
         print([[i, board[i]] for i in range(N)])
         return
-    
+
     for col in range(N):
         if is_safe(board, row, col):
             board[row] = col
             solve_nqueens(board, row + 1)
             board[row] = -1
+
 
 def main():
     if len(sys.argv) != 2:
@@ -52,6 +55,7 @@ def main():
 
     board = [-1] * N
     solve_nqueens(board, 0)
+
 
 if __name__ == "__main__":
     main()
