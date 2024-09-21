e744c07caf7358bd9467dda009aa72358a4176f6
Mery9yuka <benhaida.maryem@gmail.com>
2024-09-21 02:14:53 +0100

    fewest nb of coins

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -5,6 +5,6 @@ Main file for testing
 
 makeChange = __import__('0-making_change').makeChange
 
-print(makeChange([5, 7], 3))
+print(makeChange([1, 2, 25], 37))
 
-print(makeChange([2], 4))
+print(makeChange([1256, 54, 48, 16, 102], 1453))
--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,12 +8,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    min_c = [float('inf')] * (total + 1)
-    min_c[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i - coin >= 0:
-                min_c[i] = min(min_c[i], min_c[i - coin] + 1)
-
-    return min_c[total] if min_c[total] != float('inf') else -1
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        minCoin = 0
+        for j in coin:
+            while(total >= j):
+                minCoin += 1
+                total -= j
+        if total == 0:
+            return minCoin
+        return -1
